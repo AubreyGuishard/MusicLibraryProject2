69dcec47feb19dc03cc74755ff91b087ae8d0020
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-02 23:33:20 -0500

    Created New View Functions For Getting Music by ID + Added new urls path for Music Detail View Function

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,16 +8,35 @@ from .models import Music
 
 @api_view(['GET','POST', 'PUT', 'DELETE'])
 def music_list(request):
-    
     if request.method == 'GET':
         music = Music.objects.all()
         serializer = MusicSerializer(music, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = MusicSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
        
+@api_view(['GET'])    
+def music_detail(request, pk):
+        music = get_object_or_404(Music, pk=pk)
+        if request.method == 'GET':
+            serializer = MusicSerializer(music);
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = MusicSerializer(music, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        elif request.method == 'DELETE':
+            music.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)        
+#    try:
+#         music = Music.objects.get(pk=pk)
+#         serializer = MusicSerializer(music),
+#         return Response(serializer.data)   
+    
+#    except Music.DoesNotExist:
+#     return Response(status=status.HTTP_404_NOT_FOUND),
+        
