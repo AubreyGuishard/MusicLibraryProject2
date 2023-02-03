68fe34cd1319a9a3b7f43da8648a39bbb940146c
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-03 00:01:38 -0500

    Renamed Symbols and Classes from Music to Song

--- music/views.py
+++ music/views.py
@@ -2,33 +2,33 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MusicSerializer
-from .models import Music
+from .serializers import SongSerializer
+from .models import Song
 
 
 @api_view(['GET','POST', 'PUT', 'DELETE'])
-def music_list(request):
+def song_list(request):
     if request.method == 'GET':
-        music = Music.objects.all()
-        serializer = MusicSerializer(music, many=True)
+        music = Song.objects.all()
+        serializer = SongSerializer(music, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer = MusicSerializer(data=request.data)
+        serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
        
 @api_view(['GET', 'PUT', 'DELETE'])    
-def music_detail(request, pk):
-        music = get_object_or_404(Music, pk=pk)
+def song_detail(request, pk):
+        song = get_object_or_404(Song, pk=pk)
         if request.method == 'GET':
-            serializer = MusicSerializer(music);
+            serializer = SongSerializer(song);
             return Response(serializer.data)
         elif request.method == 'PUT':
-            serializer = MusicSerializer(music, data=request.data)
+            serializer = SongSerializer(song, data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
         elif request.method == 'DELETE':
-            music.delete()
+            song.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)        
